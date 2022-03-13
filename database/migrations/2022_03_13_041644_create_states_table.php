f3d7d2ef8cdb502c360c6930f363c80e0cffc8ea
tawfeeq <tawfeeq.tech@gmail.com>
2022-03-13 06:47:37 +0200

    02-Make Models

--- database/migrations/2022_03_13_041644_create_states_table.php
+++ database/migrations/2022_03_13_041644_create_states_table.php
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('states', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('last_name');
-            $table->string('first_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('country_id')->constrained();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('states');
     }
 };
